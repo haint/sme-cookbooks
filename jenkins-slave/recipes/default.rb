c8570ca3f3660e9bc29ef58a9c6e12c8d9a15e4d
haint <haithanh0809@gmail.com>
2014-09-03 10:34:30 +0700

    Update jenkins-master ip

--- jenkins-slave/recipes/default.rb
+++ jenkins-slave/recipes/default.rb
@@ -8,7 +8,7 @@
 
 # download file from url
 
-remote_file "#{Chef::Config[:file_cache_path]}/sme_jenkins_tools.jar" do
+remote_file "/home/ubuntu/sme_jenkins_tools.jar" do
   source "http://172.27.4.48/iso/sme-jenkins-tools.jar"
   mode "0644"
 #  not_if { ::File.exists?(java_file) }
@@ -19,9 +19,9 @@ end
 
 # Install sms_jenkins_tools
 execute "install-jar" do
-  cwd Chef::Config[:file_cache_path]
+  cwd "/home/ubuntu"
   command <<-EOF
-    /home/ubuntu/java/jdk1.7.0_51/bin/java -jar sme_jenkins_tools.jar -c
+    /home/ubuntu/java/jdk1.7.0_51/bin/java -jar sme_jenkins_tools.jar -m 172.27.4.77 -c
     # command for installing Python goes here	
     EOF
 #  not_if { ::File.exists?(java_file) }
