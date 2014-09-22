de8e40972df671c936da956576f91f08b8d05bda
haint <haithanh0809@gmail.com>
2014-09-22 16:54:20 +0700

    Update scripts

--- jenkins-slave-gui.sh
+++ jenkins-slave-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ip=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+ip=`cat ~/jenkins.master`
 command="/home/ubuntu/java/jdk1.7.0_51/bin/java -jar sme_jenkins_tools.jar -m $ip -c -g"
 echo $command
 `$command`
